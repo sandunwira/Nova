33e1659f6f05ca62de5a296f6462a6d08a1875e8
sandunwira <79461263+sandunwira@users.noreply.github.com>
2024-10-21 01:05:23 +0530

    Fixed local time display

--- src/assistant.js
+++ src/assistant.js
@@ -479,7 +479,20 @@ async function searchWeb(query) {
 function getTime() {
 	let date = new Date();
 	let hours = date.getHours();
-	hours = hours - 12;
+	switch (hours) {
+		case 0: hours = 12; break;
+		case 13: hours = 1; break;
+		case 14: hours = 2; break;
+		case 15: hours = 3; break;
+		case 16: hours = 4; break;
+		case 17: hours = 5; break;
+		case 18: hours = 6; break;
+		case 19: hours = 7; break;
+		case 20: hours = 8; break;
+		case 21: hours = 9; break;
+		case 22: hours = 10; break;
+		case 23: hours = 11; break;
+	}
 	if (hours < 10) {
 		hours = '0' + hours;
 	}
